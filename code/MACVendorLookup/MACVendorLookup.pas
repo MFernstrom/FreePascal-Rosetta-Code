a6c291a7a5a8ad9da7a709d4b3f80291bf77719e
Marcus Fernstrom <marcus@marcusfernstrom.com>
2018-12-02 20:29:41 -0500

    Adding contribution for https://rosettacode.org/wiki/MAC_Vendor_Lookup

---
+++ code/MACVendorLookup/MACVendorLookup.pas
@@ -0,0 +1,26 @@
+program MACVendorLookup;
+
+// https://rosettacode.org/wiki/MAC_Vendor_Lookup
+
+uses
+  fphttpclient;
+
+var
+  res: String;
+begin
+  if paramCount > 0 then begin
+
+    With TFPHttpClient.Create(Nil) do
+    try
+      allowRedirect := true;
+      try
+        res := Get('http://api.macvendors.com/' + ParamStr(1));
+        writeLn(res);
+      except
+        writeLn('N/A');
+      end;
+    finally
+      Free;
+    end;
+  end;
+end.
